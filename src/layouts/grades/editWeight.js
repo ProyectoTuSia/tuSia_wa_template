a841f2e95fd505c35d167b3875c285aa874db033
Julian ro <davidro2121@gmail.com>
2022-10-25 11:43:19 -0500

    creating the components and their respective pages

---
+++ src/layouts/grades/editWeight.js
@@ -0,0 +1,166 @@
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import { useState, useEffect } from "react";
+import { useNavigate, Navigate, useLocation } from "react-router-dom";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import DataTable from "examples/Tables/DataTable";
+
+// Data
+import authorsTableData from "layouts/grades/data/editWeightsTable";
+import projectsTableData from "layouts/tables/data/projectsTableData";
+import MDButton from "components/MDButton";
+import { Icon } from "@mui/material";
+import MDInput from "components/MDInput";
+
+function EditWeight() {
+  const [idWeight, setIdWeight] = useState("weight_1");
+  const [idDescription, setIdDescription] = useState("description_1");
+
+  const {
+    state: { course },
+  } = useLocation();
+  const { columns, rows: weightrows } = authorsTableData();
+  // console.log(course);
+
+  const [rows, setrows] = useState(weightrows);
+  useEffect(() => {
+    // console.log(rows, "useEffectrows");
+  }, [rows]);
+
+  const handleAdd = () => {
+    const newIdWeight = idWeight.replace(/(\d+)$/, function (match, number) {
+      return parseInt(number) + 1;
+    });
+
+    const newIdDescription = idDescription.replace(/(\d+)$/, function (match, number) {
+      return parseInt(number) + 1;
+    });
+
+    setIdWeight(newIdWeight);
+    setIdDescription(newIdDescription);
+    // console.log(document.getElementById("description_1").value, "value");
+
+    setrows(
+      rows.concat([
+        {
+          description: <MDInput name="d1" id={newIdDescription} label="Descripción" />,
+          weight: (
+            <MDInput
+              name="w1"
+              onChange={(e, { name, value }) => {
+                setrows(...rows);
+              }}
+              label="Peso"
+              id={newIdWeight}
+              type="number"
+            />
+          ),
+        },
+      ])
+    );
+
+    // console.log(temprows);
+  };
+
+  const handleChange = () => {
+    rows.filter((row) => {});
+  };
+
+  const handleSubmit = () => {
+    let gm_WeightsInput = [];
+    for (let i = 0; i <= rows.lenght + 1; i++) {
+      console.log("desde for");
+      curGrade = {
+        weight: document.getElementById(`weight_${i}`).value,
+        description: document.getElementById(`description${i}`).value,
+      };
+      gm_WeightsInput.push(curGrade);
+    }
+    console.log(gm_WeightsInput);
+  };
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="primary"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <Grid container justifyContent={"space-between"}>
+                  <MDTypography variant="h6" color="white">
+                    {course.courseName}
+                  </MDTypography>
+                  <Grid>
+                    <MDButton style={{ marginRight: "1rem" }} onClick={handleAdd}>
+                      <Icon>add</Icon>
+                    </MDButton>
+                    <MDButton onClick={handleSubmit}>
+                      <Icon>save</Icon>
+                    </MDButton>
+                  </Grid>
+                </Grid>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns, rows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+          {/* <Grid item xs={12}>
+              <Card>
+                <MDBox
+                  mx={2}
+                  mt={-3}
+                  py={3}
+                  px={2}
+                  variant="gradient"
+                  bgColor="info"
+                  borderRadius="lg"
+                  coloredShadow="info"
+                >
+                  <MDTypography variant="h6" color="white">
+                    Projects Table
+                  </MDTypography>
+                </MDBox>
+                <MDBox pt={3}>
+                  <DataTable
+                    table={{ columns: pColumns, rows: pRows }}
+                    isSorted={false}
+                    entriesPerPage={false}
+                    showTotalEntries={false}
+                    noEndBorder
+                  />
+                </MDBox>
+              </Card>
+            </Grid> */}
+        </Grid>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default EditWeight;
