a4e69fccaeff9a0af496e66778718f602ad972fd
Julian ro <davidro2121@gmail.com>
2022-11-08 10:53:51 -0500

    hot fixes

--- src/layouts/Search/data/ExternalSubjectTableData.js
+++ src/layouts/Search/data/ExternalSubjectTableData.js
@@ -12,29 +12,56 @@ import InboxIcon from "@mui/icons-material/MoveToInbox";
 import { Checkbox, Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
 import DataTable from "examples/Tables/DataTable";
 import { useQuery } from "urql";
+import soap from "soap";
 
-const DATA_QUERY_SUBJECTS = `
-query GetSubject {
-  getSubject {
-    Id_subject
-    Name_subject
-    Typology
-    Credits
-    Description
-    Id_career
-    Id_condition
-  }
-}`;
+// const DATA_QUERY_SUBJECTS = `
+// query GetSubject {
+//   getSubject {
+//     Id_subject
+//     Name_subject
+//     Typology
+//     Credits
+//     Description
+//     Id_career
+//     Id_condition
+//   }
+// }`;
 
 function SubjectsData(searchid, searchname) {
-  const [result, reexecuteQuery] = useQuery({
-    query: DATA_QUERY_SUBJECTS,
-    requestPolicy: "network-only",
+  const url = "https://interfaceSOAPSIA2B.crvargasm.repl.co/wsdl?wsdl";
+  const [externalData, setExternalData] = useState();
+
+  // Create client
+  soap.createClient(url, function (err, client) {
+    if (err) {
+      throw err;
+    }
+    /*
+     * Parameters of the service call: they need to be called as specified
+     * in the WSDL file
+     */
+
+    var args = {
+      id: 10,
+    };
+
+    // call the service
+    client.Documents(args, function (err, res) {
+      if (err) throw err;
+      // print the service returned result
+      console.log(res);
+      setExternalData(res);
+    });
   });
 
-  const { data, fetching, error } = result;
+  // const [result, reexecuteQuery] = useQuery({
+  //   query: DATA_QUERY_SUBJECTS,
+  //   requestPolicy: "network-only",
+  // });
 
-  if (fetching) {
+  // const { data, fetching, error } = result;
+
+  if (!externalData) {
     return {
       columns: [],
       rows: [],
@@ -48,44 +75,74 @@ function SubjectsData(searchid, searchname) {
     };
   }
 
-  const listSubject =
-    !searchid && !searchname
-      ? data.getSubject
-      : data.getSubject.filter(
-          (dato) =>
-            (dato.Id_subject.toString().includes(searchid) && searchid) ||
-            (dato.Name_subject.toLowerCase().includes(searchname.toLocaleLowerCase()) && searchname)
-        );
+  // const listSubject =
+  //   !searchid && !searchname
+  //     ? data.getSubject
+  //     : data.getSubject.filter(
+  //         (dato) =>
+  //           (dato.id.toString().includes(searchid) && searchid) ||
+  //           (dato.nombre.toLowerCase().includes(searchname.toLocaleLowerCase()) && searchname)
+  //       );
 
   //listSchedulesOfGroup.map((element) => props.selectedSchedules.push(element.schedule.id));
 
   return {
     columns: [
-      { Header: "ID", accessor: "Id_subject", align: "right" },
-      { Header: "Nombre Materia", accessor: "Name_subject", align: "center" },
-      { Header: "Creditos", accessor: "Credits", align: "left" },
-      { Header: "Typologia", accessor: "Typology", align: "left" },
-      { Header: "Sede", accessor: "Sede", align: "left" },
-      { Header: "Nivel de estudio", accessor: "Nivel_estudio", align: "left" },
-      { Header: "Facultad", accessor: "Id_career", align: "left" },
-      { Header: "Descripcion", accessor: "Descripcion", align: "left" },
-      { Header: "Pre-Requisitos", accessor: "Descripcion", align: "left" },
-      { Header: "Codigo", accessor: "Id_subject", align: "left" },
+      { Header: "ID", accessor: "id", align: "right" },
+      { Header: "Nombre Materia", accessor: "nombre", align: "center" },
+      { Header: "Creditos", accessor: "creditos", align: "left" },
+      { Header: "Typologia", accessor: "tipologia", align: "left" },
+      { Header: "Sede", accessor: "sede", align: "left" },
+      { Header: "Nivel de estudio", accessor: "nivelestudio", align: "left" },
+      { Header: "Facultad", accessor: "facultad", align: "left" },
+      { Header: "Descripcion", accessor: "descripcion", align: "left" },
+      { Header: "Pre-Requisitos", accessor: "prerequisitos", align: "left" },
+      { Header: "Codigo", accessor: "codigo", align: "left" },
     ],
     rows: listSubject.map((element) => ({
-      Id_subject: (
+      id: (
         <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-          {element.Id_subject}
+          {element.id}
         </MDTypography>
       ),
-      Name_subject: (
+      creditos: (
         <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-          {element.Name_subject}
+          {element.creditos}
         </MDTypography>
       ),
-      Typology: (
+      tipologia: (
         <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-          {element.Typology}
+          {element.tipologia}
+        </MDTypography>
+      ),
+      sede: (
+        <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+          {element.sede}
+        </MDTypography>
+      ),
+      nivelestudio: (
+        <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+          {element.nivelestudio}
+        </MDTypography>
+      ),
+      facultad: (
+        <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+          {element.facultad}
+        </MDTypography>
+      ),
+      descripcion: (
+        <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+          {element.descripcion}
+        </MDTypography>
+      ),
+      prerequisitos: (
+        <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+          {element.prerequisitos}
+        </MDTypography>
+      ),
+      code: (
+        <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+          {element.code}
         </MDTypography>
       ),
     })),
